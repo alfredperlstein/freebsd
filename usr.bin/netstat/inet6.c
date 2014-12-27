f1dfe833b7eaf8afc1942d0fa193d3a7775a92d5
Alfred Perlstein <alfred@freebsd.org>
2014-12-26 21:41:30 -0800

    hex address needs to be quoted

--- usr.bin/netstat/inet6.c
+++ usr.bin/netstat/inet6.c
@@ -505,7 +505,7 @@ ip6_stats(u_long off, const char *name, int af1 __unused, int proto __unused)
 			  "{N:/global%s}\n");\
 			break;\
 		default:\
-			xo_emit("\t\t{ke:name/%x}{:count/%ju} " \
+			xo_emit("\t\t{qke:name/%x}{:count/%ju} " \
 				"addresses scope=%x\n",\
 				i, (uintmax_t)ip6stat.s, i);	   \
 		}\
